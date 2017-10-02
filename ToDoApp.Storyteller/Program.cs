2d2435df004e4e4332ad0487ba13cc37ef18dc1f
Kelsey Brisson <klbrisson@gmail.com>
2017-10-01 19:59:24 -0600

    Include storyteller

---
+++ ToDoApp.Storyteller/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using StoryTeller;
+
+namespace ToDoApp.Storyteller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        StorytellerAgent.Run(args);
+		}
+    }
+}
